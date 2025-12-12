e4f41571a535a317fff940f93b87c6183fe596ab
im-prathamesh-dev <prathamesh0755@gmail.com>
2025-12-12 21:32:28 +0530

    route added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
-
+app.use('/api/users', require('./routes/users'));
 
 
 app.listen(port, () => {
